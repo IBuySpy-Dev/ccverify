7044ad1b151a7b2122b736300dc8ce4470610a7f
ibuyspy <a-ivega@ibuyspy.net>
2026-05-08 23:47:23 -0400

    fix(tests): replace invalid Luhn Amex test number with Stripe 378282246310005

--- tests/CCVerify.Tests/LuhnAlgorithmTests.cs
+++ tests/CCVerify.Tests/LuhnAlgorithmTests.cs
@@ -8,7 +8,7 @@ public class LuhnAlgorithmTests
     [Theory]
     [InlineData("4532015112830366", true)]   // Valid Visa
     [InlineData("5425233430109903", true)]   // Valid MasterCard
-    [InlineData("374251018720955", true)]    // Valid Amex
+    [InlineData("378282246310005",  true)]   // Valid Amex 37 (Stripe test number)
     [InlineData("6011111111111117", true)]   // Valid Discover
     [InlineData("1234567890123456", false)]  // Invalid
     [InlineData("4532015112830367", false)]  // Visa with bad check digit
